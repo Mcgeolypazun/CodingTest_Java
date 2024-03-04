3428b91182b26f310f7ce2979eb994fa1b11c3af
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-03-04 23:32:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.31 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,17 +2,44 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        int count = 0;
+        Map<String,Integer> a = new HashMap<>();
         
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        for(int i=0;i<participant.length;i++){
+            a.put(participant[i],a.getOrDefault(participant[i],0) + 1);
+        }
         
+        Set<String> setMap = a.keySet();
         for(int i=0;i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
-                answer = participant[i];
-                return answer;
+            if(a.containsKey(completion[i])){
+                a.put(completion[i],a.get(completion[i])-1);
+                if(a.get(completion[i]) == 0){
+                    a.remove(completion[i]);
+                }
             }
         }
-        return participant[participant.length-1];
+        
+        
+        for(String c : setMap){
+            if(a.get(c) > 0){
+                answer = c;
+                break;
+            }
+        }
+        
+        
+        return answer;
+        
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
+        
+//         for(int i=0;i<completion.length;i++){
+//             if(!participant[i].equals(completion[i])){
+//                 answer = participant[i];
+//                 return answer;
+//             }
+//         }
+//         return participant[participant.length-1];
         
         
 //         for(int i=0;i<completion.length;i++){
