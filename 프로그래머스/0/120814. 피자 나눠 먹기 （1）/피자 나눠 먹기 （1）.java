318e3f9ddd81fb5cfa14c8ae1caeda418312ba9a
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-03-07 22:16:15 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.20 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        answer = (int)Math.ceil((double)n/7);
+        
+        return answer;
+    }
+}
