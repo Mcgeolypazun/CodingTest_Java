97003cf674220cabccee70615a1fc5b6dd1b2ad2
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-03-17 01:21:18 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 8.24 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
         for(int i=0;i<my_string.length();i++){
-            char a = my_string.charAt(i);
-            for(int j=0;j<n;j++){
-                answer += String.valueOf(a);
-            }
+            answer.append((my_string.charAt(i)+"").repeat(n));
         }
         
-        return answer;
+        return answer.toString();
     }
 }
