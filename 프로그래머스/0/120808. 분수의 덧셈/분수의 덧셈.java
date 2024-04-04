b4df500800a35e5f49ec5f408ebeecf6c389fa89
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-04-04 12:51:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.18 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,6 +1,27 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        
+        int tmp1 = denom1;
+        int tmp2 = denom2;
+        denom1 *= tmp2;
+        denom2 *= tmp1;
+        
+        numer1 *= tmp2;
+        numer2 *= tmp1;
+        
+        int a = numer1 + numer2;
+        
+        for(int i=a;i>=2;i--){
+            if(a % i ==0 && denom1 % i == 0){
+                a /= i;
+                denom1 /= i;
+                break;
+            }
+        }
+        answer[0] = a;
+        answer[1] = denom1;
+        
         return answer;
     }
 }
