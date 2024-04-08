a3420390ab3062e2b77ed03f82fe27846fa42e24
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-04-08 10:16:21 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.12 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i=left;i<=right;i++){
+            int count = 0;
+            for(int j=1;j<=Math.sqrt(i);j++){
+                if(i % j == 0){
+                    count++;
+                    if(j != i/j){
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+            if(count % 2 == 0){
+                answer += i;
+            }
+            else{
+                answer -= i;
+            }
+        }
+        
+        return answer;
+    }
+}
