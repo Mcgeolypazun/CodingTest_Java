727c6e3467c33901c0b99b7d26c37dc07ba90481
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-04-12 23:58:27 +0900

    [level 0] Title: 문자열 섞기, Time: 1.37 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String str1, String str2) {
+        String answer = "";
+        int length = str1.length()+str2.length();
+        int index = 0;
+        for(int i=0;i<length;i++){
+            if(i % 2 == 0){
+                answer += String.valueOf(str1.charAt(index));
+            }
+            else{
+                answer += String.valueOf(str2.charAt(index));
+                index++;
+            }
+        }
+        
+        return answer;
+    }
+}
