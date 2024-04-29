d8b79944a4e854fb224a0029de34f39e7b211be2
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-04-29 10:52:29 +0900

    [level 0] Title: 수 조작하기 2, Time: 712.39 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,20 +1,23 @@
 class Solution {
     public String solution(int[] numLog) {
-        StringBuilder result = new StringBuilder();
+        String answer = "";
         
-        for (int i = 1; i < numLog.length; i++) {
-            int diff = numLog[i] - numLog[i - 1];
-            if (diff == 1) {
-                result.append("w");
-            } else if (diff == -1) {
-                result.append("s");
-            } else if (diff == 10) {
-                result.append("d");
-            } else if (diff == -10) {
-                result.append("a");
+        for(int i=1;i<numLog.length;i++){
+            int diff = numLog[i] - numLog[i-1];
+            if(diff == 1){
+                answer += "w";
+            }
+            else if(diff == -1){
+                answer += "s";
+            }
+            else if(diff == 10){
+                answer += "d";
+            }
+            else if(diff == -10){
+                answer += "a";
             }
         }
         
-        return result.toString();
+        return answer;
     }
 }
