e5520b3b0d49ddfa54bba9829de32febff1d4e6d
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-04-30 10:40:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        Map<String,Integer> map = new HashMap<>();
+        int count = 1;
+        int Human = 1;
+        
+        map.put(words[0],Human++);
+        
+        for(int i=1;i<words.length;i++){
+            char pre = words[i-1].charAt(words[i-1].length()-1);
+            if(pre != words[i].charAt(0)){
+                answer[0] = Human;
+                answer[1] = count;
+                break;
+            }
+            if(map.containsKey(words[i])){
+                answer[0] = Human;
+                answer[1] = count;
+                break;
+            }
+            else{
+                Human++;
+                map.put(words[i],Human);
+            }
+            
+            if(Human > n){
+                Human = 1;
+                count++;
+            }
+        }
+        
+
+        return answer;
+    }
+}
