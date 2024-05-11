5406ccc9b40ab761b23817b6f9366f889a1d7a4f
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-05-11 14:30:27 +0900

    [level 0] Title: 배열 만들기 3, Time: 5.26 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,21 +1,7 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        int a = intervals[0][0];
-        int b = intervals[0][1];
-        int c = intervals[1][0];
-        int d = intervals[1][1];
-        int len = (b-a)+(d-c)+2;
-        int ansIdx = 0;
-        int[] answer = new int[len];
-        
-        
-        for(int i=a;i<=b;i++){
-            answer[ansIdx++] = arr[i];
-        }
-        for(int i=c;i<=d;i++){
-            answer[ansIdx++] = arr[i];
-        }
-        
+        int[] answer = Arrays.stream(intervals).flatMapToInt(ints -> Arrays.stream(Arrays.copyOfRange(arr,ints[0],ints[1]+1))).toArray();
         return answer;
     }
 }
