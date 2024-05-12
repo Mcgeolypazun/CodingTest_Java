0b0947601660aac1c0760b970f152d7736ddb90c
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-05-13 06:24:15 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = new String[0];
+        
+        for(int i=0;i<str_list.length;i++){
+            if(str_list[i].equals("l")){
+                answer = new String[i];
+                answer = Arrays.copyOfRange(str_list,0,i);
+                
+                break;
+            }
+            else if(str_list[i].equals("r")){
+                answer = new String[str_list.length - i - 1];
+                answer = Arrays.copyOfRange(str_list,i+1,str_list.length);
+                break;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
