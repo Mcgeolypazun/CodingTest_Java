84fa8f12cfc7dad58af6a4ad7187e7a5e0645027
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-05-13 11:55:47 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,26 +1,15 @@
-import java.util.*;
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
         String answer = "";
-        Map<String,String> map = new HashMap<>();
-        
-        for(int i=0;i<db.length;i++){
-            map.put(db[i][0],db[i][1]);
-        }
-        
-        if(map.containsKey(id_pw[0])){
-            if(map.get(id_pw[0]).equals(id_pw[1])){
-                answer = "login";
-            }
-            else{
-                answer = "wrong pw";
+
+        for(int i = 0; i < db.length; i++){
+            if(db[i][0].equals(id_pw[0])){
+                if(db[i][1].equals(id_pw[1])){
+                    return "login";
+                }
+                return "wrong pw";
             }
         }
-        else{
-            answer = "fail";
-        }
-        
-        
-        return answer;
+        return "fail";
     }
 }
