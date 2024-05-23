abfab26dc6829f2f4562db4e22ee81a32da94680
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-05-23 10:56:12 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int[] answer = new int[2];
+        int idx = -1;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<array.length;i++){
+            if(max < array[i]){
+                idx = i;
+                max = array[i];
+            }
+        }
+        answer[0] = max;
+        answer[1] = idx;
+        
+        return answer;
+    }
+}
