f4f8902da9368c38e1ac3021fbb437828c9e3db5
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-05-31 22:21:53 +0900

    [level 1] Title: 옹알이 (2), Time: 4.21 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,7 +3,7 @@ class Solution {
         int answer = 0;
         
         for(String baby : babbling){
-            String str = baby.replaceAll("(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))","*");
+            String str = baby.replaceAll("aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma)","*");
             String str2 = str.replaceAll("[a-z]","-");
             if(!str2.contains("-") && str2.contains("*")) answer++;
         }
