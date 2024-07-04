a95f416c2a42c53598b35648755cce07d6597271
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-07-04 22:26:53 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.12 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder result = new StringBuilder();
+        int num = t * m;
+        int curNum = 0;
+        int loop = 0;
+        while(loop != num){
+            String changeableNum = Integer.toString(curNum++,n);
+            for(int i=0;i<changeableNum.length();i++){
+                if(loop != 0 && loop % (m)-p+1 == 0){
+                    result.append(changeableNum.toUpperCase().charAt(i));
+                }
+                else if(loop == p-1){
+                    result.append(changeableNum.toUpperCase().charAt(i));
+                }
+                loop++;
+                if(loop == num) break;
+            }
+        }
+
+        return result.toString();
+    }
+}
