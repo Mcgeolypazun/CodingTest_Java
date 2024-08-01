db7a9aebc12e4cae874818c7833eee40562158d2
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-08-02 02:36:27 +0900

    [level 2] Title: 주차 요금 계산, Time: 30.95 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -3,66 +3,50 @@ import java.time.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
+        int basicHour = fees[0];
+        int basicFee = fees[1];
+        int overTime = fees[2];
+        int overFee = fees[3];
         
-        int baseTime = fees[0];
-        int baseFee = fees[1];
-        int unitTime = fees[2];
-        int unitFee = fees[3];
-
-        
-        Map<String, Integer> parkingTime = new HashMap<>();
-        Map<String, LocalTime> inTime = new HashMap<>();
+        Map<String, Integer> carHours = new TreeMap<>();
+        Map<String, LocalTime> enterCar = new HashMap<>();
         
         for (String record : records) {
             String[] parts = record.split(" ");
             LocalTime time = LocalTime.parse(parts[0]);
-            String carNumber = parts[1];
-            String action = parts[2];
+            String carNum = parts[1];
+            String inOut = parts[2];
             
-            if (action.equals("IN")) {
-                inTime.put(carNumber, time);
+            if (inOut.equals("IN")) {
+                enterCar.put(carNum, time);
             } else {
-                LocalTime entryTime = inTime.remove(carNumber);
-                int parkedMinutes = (int) Duration.between(entryTime, time).toMinutes();
-                parkingTime.put(carNumber, parkingTime.getOrDefault(carNumber, 0) + parkedMinutes);
+                LocalTime inTime = enterCar.remove(carNum);
+                int minutes = (int) Duration.between(inTime, time).toMinutes();
+                carHours.put(carNum, carHours.getOrDefault(carNum, 0) + minutes);
             }
         }
         
-        
-        LocalTime endTime = LocalTime.of(23, 59);
-        for (Map.Entry<String, LocalTime> entry : inTime.entrySet()) {
-            String carNumber = entry.getKey();
-            LocalTime entryTime = entry.getValue();
-            int parkedMinutes = (int) Duration.between(entryTime, endTime).toMinutes();
-            parkingTime.put(carNumber, parkingTime.getOrDefault(carNumber, 0) + parkedMinutes);
+        // 처리되지 않은 IN 기록을 23:59 출차로 간주
+        LocalTime endOfDay = LocalTime.of(23, 59);
+        for (String carNum : enterCar.keySet()) {
+            LocalTime inTime = enterCar.get(carNum);
+            int minutes = (int) Duration.between(inTime, endOfDay).toMinutes();
+            carHours.put(carNum, carHours.getOrDefault(carNum, 0) + minutes);
         }
         
-        
-        Map<String, Integer> parkingFee = new TreeMap<>();
-        for (Map.Entry<String, Integer> entry : parkingTime.entrySet()) {
-            String carNumber = entry.getKey();
-            int totalTime = entry.getValue();
-            int fee = calculateFee(totalTime, baseTime, baseFee, unitTime, unitFee);
-            parkingFee.put(carNumber, fee);
-        }
-        
-        
-        int[] answer = new int[parkingFee.size()];
-        int i = 0;
-        for (int fee : parkingFee.values()) {
-            answer[i++] = fee;
+        // 요금 계산
+        int[] answer = new int[carHours.size()];
+        int index = 0;
+        for (int totalTime : carHours.values()) {
+            if (totalTime <= basicHour) {
+                answer[index++] = basicFee;
+            } else {
+                int overMinutes = totalTime - basicHour;
+                int overUnits = (int) Math.ceil(overMinutes / (double) overTime);
+                answer[index++] = basicFee + overUnits * overFee;
+            }
         }
         
         return answer;
-    }
-    
-    private int calculateFee(int totalTime, int baseTime, int baseFee, int unitTime, int unitFee) {
-        if (totalTime <= baseTime) {
-            return baseFee;
-        } else {
-            int extraTime = totalTime - baseTime;
-            int extraUnits = (int) Math.ceil((double) extraTime / unitTime);
-            return baseFee + extraUnits * unitFee;
-        }
     }
 }
