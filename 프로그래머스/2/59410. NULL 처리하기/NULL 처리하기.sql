9b3554ebb06fbef253ad7db1d863e200fbd6b3b3
Sunghyuk Jang <79851594+Mcgeolypazun@users.noreply.github.com>
2024-08-23 21:41:57 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,4 +1,14 @@
 -- 코드를 입력하세요
-SELECT ANIMAL_TYPE,  if(NAME IS NULL,"No name", NAME) NAME, SEX_UPON_INTAKE
+SELECT ANIMAL_TYPE, CASE WHEN NAME IS NULL THEN 'No name' else name
+end as name, SEX_UPON_INTAKE
 from animal_ins
 order by animal_id
+
+# SELECT ANIMAL_TYPE, 
+#        CASE 
+#            WHEN NAME IS NULL THEN 'No name' 
+#            ELSE NAME 
+#        END AS NAME, 
+#        SEX_UPON_INTAKE 
+# FROM ANIMAL_INS
+# ORDER BY ANIMAL_ID
